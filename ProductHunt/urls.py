8df69c4a974819eae4ae44cf3f6e02aa46a0c0f8
suyash1798 <suyashtiwari1798@gmail.com>
2018-10-07 04:22:34 +0530

    accounts model completed

--- ProductHunt/urls.py
+++ ProductHunt/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from  django.contrib import admin
 from django.urls import path
 
@@ -20,5 +21,6 @@ from products import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('accounts/', include('accounts.urls'))
 ]
